d7b7860e4b9cf277452a6cb9623c70abb919b8dd
Marouane Ahal <d144013152@gmail.com>
2022-07-19 15:31:09 +0100

    create main driver app

---
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,28 @@
+package com.luv2code.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read json file and map/convert to java pojo: data/sample-lite.json
+            Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("First name: " + theStudent.getFirstName());
+            System.out.println("Last name: " + theStudent.getLastName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
